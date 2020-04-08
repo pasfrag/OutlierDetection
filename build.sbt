c15e235600752e47f79eaed6f01768c161081c23
Paschalis Frangidis <pascal.frag@gmail.com>
2020-04-08 11:41:36 +0300

    Data filtering

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "OutlierDetection"
 version := "0.1"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.4"
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,28 @@
-class main {
+import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.functions.col
+
+
+object main {
+
+    def main(args: Array[String]): Unit = {
+
+        val spark: SparkSession = SparkSession.builder().config("spark.master", "local[*]").getOrCreate()
+        val sc: SparkContext = spark.sparkContext
+        val sqlContext: SQLContext = spark.sqlContext
+        LogManager.getRootLogger.setLevel(Level.ERROR)
+
+        val dataDF = spark.read.option("header", "false").csv("C:\\Users\\pasca\\Desktop\\Projects\\Spark_Projects\\OutlierDetection\\data\\data-example.txt")
+
+        val doubleDF = dataDF
+          .withColumn("x", col("_c0").cast("Double"))
+          .withColumn("y", col("_c1").cast("Double"))
+          .select("x", "y")
+
+        val filteredDF = doubleDF.filter(row => (row(0) != null && row(1) != null))
+//        filteredDF.show(7653)
+
+    }
 
 }
